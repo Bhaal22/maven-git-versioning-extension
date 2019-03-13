486909accba35e6f122c5819630dd75704578b86
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-03-14 00:53:19 +0100

    Update GitVersioningPomReplacementMojo.java

--- src/main/java/me/qoomon/maven/gitversioning/GitVersioningPomReplacementMojo.java
+++ src/main/java/me/qoomon/maven/gitversioning/GitVersioningPomReplacementMojo.java
@@ -41,6 +41,7 @@ public class GitVersioningPomReplacementMojo extends AbstractMojo {
         try {
             getLog().debug(currentProject.getModel().getArtifactId() + "remove this plugin from model");
             currentProject.getOriginalModel().getBuild().removePlugin(asPlugin());
+                
             File gitVersionedPomFile = new File(currentProject.getBuild().getDirectory(), GIT_VERSIONED_POM_FILE_NAME);
             getLog().debug(currentProject.getArtifact() + " replace project pom file with " + gitVersionedPomFile);
             ModelUtil.writeModel(gitVersionedPomFile, currentProject.getOriginalModel());
